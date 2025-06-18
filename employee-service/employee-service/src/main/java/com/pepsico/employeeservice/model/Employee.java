e40aaf4370b3a49df90769b8bd3c8530e9394c02
patelapurva70@gmail.com <patelapurva70@gmail.com>
2025-06-18 14:36:40 +0530

    fix for auto increment id

--- employee-service/employee-service/src/main/java/com/pepsico/employeeservice/model/Employee.java
+++ employee-service/employee-service/src/main/java/com/pepsico/employeeservice/model/Employee.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Employee {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String department;
